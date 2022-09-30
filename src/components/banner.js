7ea300f41d9e50e36c6a021f17f5005854a21f2c
reganbp <reganbp@gmail.com>
2022-09-29 20:59:04 -0400

    Add banner

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,16 +1,19 @@
-import React from "react";
-import { Container, Row } from "react-bootstrap";
+import React, { Fragment } from "react";
 import banner from "../images/hops-brew.jpg";
+import classes from "./banner.module.css";
 
 const Banner = () => {
   return (
-    <>
-      <Container fluid>
-        <Row>
-          <img className="banner-image" src={banner} alt="banner" />
-        </Row>
-      </Container>{" "}
-    </>
+    <Fragment>
+      <header className={classes.header}>
+        <h1> Brews Traveller </h1>
+        <p style={{ padding: "5% 20%%" }}> Veni, Vedi, Bibi</p>
+      </header>{" "}
+      <div className={classes["main-image"]}>
+        <img src={banner} alt="table" />
+        <h1>Brew Traveller</h1>
+      </div>
+    </Fragment>
   );
 };
 
