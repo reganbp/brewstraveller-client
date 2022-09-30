c0aa66ad203d6e6e23c38f2cec5894cc0cfce969
reganbp <reganbp@gmail.com>
2022-09-29 20:59:36 -0400

    Update App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { Fragment } from "react";
 import "./App.css";
 import Banner from "./components/banner.js";
+import Login from "./components/login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Banner />
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <Banner />
+    //   </header>
+    // </div>
+    <Fragment>
+      <Banner />
+      <Login />
+    </Fragment>
   );
 }
 
