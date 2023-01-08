8d42fad05cf3bf2c36975f92f9d24cfbc5350d50
reganbp <reganbp@gmail.com>
2023-01-08 15:36:44 -0500

    add login page and refactor

---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "./components/banner";
+import LoginForm from "./components/loginForm";
+
+const Login = () => {
+  return (
+    <>
+      <Banner />
+      <LoginForm />
+    </>
+  );
+};
+
+export default Login;
