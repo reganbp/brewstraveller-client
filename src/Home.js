9cf9f0ea596331b3bef188051544e6e6d158b6b0
reganbp <reganbp@gmail.com>
2023-01-11 14:50:44 -0500

    Add basic navbar  - not functional  - not named correctly

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HomeBanner from "./components/homeBanner";
+import TopNav from "./components/navbar";
 
 const Home = () => {
   return (
     <>
       <HomeBanner />
+      <TopNav />
       <h1>Home</h1>
     </>
   );
