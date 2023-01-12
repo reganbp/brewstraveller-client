128abd8205a7d6c66b4c0c41e86670fab3b6074e
reganbp <reganbp@gmail.com>
2023-01-11 21:55:52 -0500

    Add brewcard component Add axios post call to get data  - fake data

--- src/components/brewCard.js
+++ src/components/brewCard.js
@@ -1,16 +1,14 @@
+import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
-const BrewCard = () => {
+const BrewCard = ({ title, body }) => {
   return (
     <Card style={{ width: "18rem" }}>
       <Card.Img variant="top" />
       <Card.Body>
-        <Card.Title> Card Title </Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card 's content.
-        </Card.Text>
+        <Card.Title> {title} </Card.Title>
+        <Card.Text>{body}</Card.Text>
         <Button variant="primary"> Go somewhere </Button>
       </Card.Body>
     </Card>
