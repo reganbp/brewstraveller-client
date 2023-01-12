5d7a7ba187d8d9239f93f323063c55ff7e82fbb2
reganbp <reganbp@gmail.com>
2023-01-12 15:01:15 -0500

    again

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -11,7 +11,7 @@ const TopNav = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#/">Home</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="#/login">Login</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
