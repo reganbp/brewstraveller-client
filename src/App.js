7d19f22f3b10c69479473c17c0eb594bc2ed56bb
reganbp <reganbp@gmail.com>
2023-01-11 22:12:32 -0500

    Add hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import "./App.css";
 import Login from "./Login";
 import Home from "./Home";
@@ -15,17 +15,31 @@ function App() {
     //   <Banner />
     //   <Login />
     // </Fragment>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        {/* <Route index element={<Home />} /> */}
-        <Route path="/home" element={<Home />} />
-        {/* <Route path="blogs" element={<Blogs />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} /> */}
-        {/* </Route> */}
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Login />} />
+    //     {/* <Route index element={<Home />} /> */}
+    //     <Route path="/home" element={<Home />} />
+    //     {/* <Route path="blogs" element={<Blogs />} />
+    //       <Route path="contact" element={<Contact />} />
+    //       <Route path="*" element={<NoPage />} /> */}
+    //     {/* </Route> */}
+    //   </Routes>
+    // </BrowserRouter>
+    <HashRouter basename="/">
+      <div>
+        <ul>
+          <a href="/#/">
+            <li>Login</li>
+          </a>
+          <a href="/#/Home">
+            <li>About</li>
+          </a>
+        </ul>
+        <Route exact path="/" component={Login} />
+        <Route path="/home" component={Home} />
+      </div>
+    </HashRouter>
   );
 }
 
